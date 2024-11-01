2d6d1cac373cfac3b6faaeb0df94da3e378aa1c6
Carlos <carlos.cuesta.quintas@alumnojoyfe.iepgroup.es>
2024-11-01 03:21:28 +0100

    Añadidos los estilos de los movimientos

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -3,14 +3,21 @@
         <div>Atrás</div>
     </router-link>
     <div>{{ id }}</div>
-    <div class="caja" v-for="movimiento in movimientos">
+    <div class="caja" :class="datos.tipos[i-1]" v-for="i in datos.nombres.length">
+        <div>{{ datos.nombres[i-1] }}</div>
+        <div>{{ datos.tipos[i-1] }}</div>
+    </div>
+    <!-- <div class="caja" v-for="movimiento in movimientos">
+        <div>{{ movimiento.move.name }}</div>
+        <div>{{ movimiento.move.url }}</div>
         <div v-for="elemento in movimiento">
             {{ elemento }}
         </div>
-    </div>
+    </div> -->
 </template>
 
 <style scoped>
+    @import '../assets/pokemon-page.scss';
     div {
         margin-top: 20px;
     }
@@ -27,13 +34,28 @@
 
     let route = useRoute();
     let id = ref(route.params.id);
-    let movimientos = ref([]);
+
+    let datos = ref(
+        {
+            "nombres": [],
+            "tipos": []
+        }
+    );
 
     onBeforeMount(async () => {
         let data = await fetch("https://pokeapi.co/api/v2/pokemon/" + id.value);
         data = await data.json();
-        movimientos.value = data.moves;
-        console.log(data);
-        console.log(movimientos);
+        let movimientos = data.moves;
+
+        for (let i = 0; i < movimientos.length; i++) {
+            fetch(movimientos[i].move.url)
+            .then(response => response.json())
+            .then((data) => {
+                datos.value.nombres.push(data.name);
+                datos.value.tipos.push(data.type.name);
+            });
+        }
+
+        console.log(datos.value.tipos);
     });
 </script>
