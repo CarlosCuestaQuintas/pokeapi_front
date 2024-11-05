7c6d487890b420678aa7c0901296000eb6351a07
Carlos <carlos.cuesta.quintas@alumnojoyfe.iepgroup.es>
2024-11-05 12:56:25 +0100

    Añadidos los sprites y una plantilla de ventana emergente

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -9,7 +9,38 @@
     </div> -->
 
     <div class="contenedor">
-
+        <div class="sprite" v-if="sprites.back_default">
+            <div class="subtitulo mayus">trasera por defecto</div>
+            <img :src=sprites.back_default>
+        </div>
+        <div class="sprite" v-if="sprites.back_female">
+            <div class="subtitulo mayus">trasera hembra</div>
+            <img :src=sprites.back_female>
+        </div>
+        <div class="sprite" v-if="sprites.back_shiny">
+            <div class="subtitulo mayus">trasera varicolor</div>
+            <img :src=sprites.back_shiny>
+        </div>
+        <div class="sprite" v-if="sprites.back_shiny_female">
+            <div class="subtitulo mayus">trasera hembra varicolor</div>
+            <img :src=sprites.back_shiny_female>
+        </div>
+        <div class="sprite" v-if="sprites.front_default">
+            <div class="subtitulo mayus">frontal por defecto</div>
+            <img :src=sprites.front_default>
+        </div>
+        <div class="sprite" v-if="sprites.front_female">
+            <div class="subtitulo mayus">frontal hembra</div>
+            <img :src=sprites.front_female>
+        </div>
+        <div class="sprite" v-if="sprites.front_shiny">
+            <div class="subtitulo mayus">frontal varicolor</div>
+            <img :src=sprites.front_shiny>
+        </div>
+        <div class="sprite" v-if="sprites.front_shiny_female">
+            <div class="subtitulo mayus">frontal hembra varicolor</div>
+            <img :src=sprites.front_shiny_female>
+        </div>
     </div>
 
     <div class="contenedor">
@@ -17,8 +48,15 @@
         :nombre=datos.nombres[i-1]
         :tclassipo=datos.tipos[i-1]
         :pp=datos.pps[i-1]
-        :poder=datos.poderes[i-1] />
+        :poder=datos.poderes[i-1]
+        v-on:click="prueba(
+            datos.nombres[i-1],
+            datos.tipos[i-1])" />
     </div>
+
+    <template class="transparencia" v-on:click="prueba()">
+        <div class="caja-flotante undefined">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum tenetur dolores laboriosam commodi dolorum veritatis vitae rem corrupti, modi tempora quod labore repellat recusandae impedit minima totam? Rerum, nobis adipisci?</div>
+    </template>
 </template>
 
 <style scoped>
@@ -27,7 +65,7 @@
 
 <script setup>
     import MovimientoComponent from '@/components/MovimientoComponent.vue';
-    import { ref, onBeforeMount } from 'vue';
+    import { ref, onBeforeMount, onMounted } from 'vue';
     import { useRoute } from 'vue-router';
 
     let route = useRoute();
@@ -41,6 +79,8 @@
             "poderes": []
         }
     );
+
+    let sprites = ref({});
 
     onBeforeMount(async () => {
         let data = await fetch("https://pokeapi.co/api/v2/pokemon/" + id.value);
@@ -57,5 +97,25 @@
                 datos.value.poderes.push(data.power);
             });
         }
-    });
+        sprites.value = data.sprites;
+    });    
+
+    // Funciones (bajarlo y quitar las líneas de depuración)
+    function prueba(nombre, tipo) {
+        let t = document.querySelector(".transparencia");
+        console.log(t);
+        nombre ? console.log("texto") : console.log("no texto");
+        (t.style.display=="none") ? console.log("sí") : console.log("no");
+        (t.style.display=="none") ? t.style.display = "block" : t.style.display = "none";
+        let c = document.querySelector(".caja-flotante");
+        console.log(c.classList);
+        // c.classList.add(tipo);
+        c.classList.replace(c.classList[1], tipo);
+        console.log(c.classList);
+        // t.style.display = "block";
+    }
+
+    onMounted(() => {
+        document.querySelector(".transparencia").style.display = "none";
+    })
 </script>
